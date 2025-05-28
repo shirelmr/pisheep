1e24b0bc2cb18e923dbac894c17e02efecea7203
Aristides <arisnietog@gmail.com>
2025-05-28 14:46:46 -0600

    imagenes game.php 3

--- main/arena/game.php
+++ main/arena/game.php
@@ -16,59 +16,17 @@
       width: 100vw;
       overflow: hidden;
     }
-
-    .ram {
-      position: absolute;
-      width: 20%;
-    }
-
-    .ram.left {
-      left: 5%;
-      top: 60%;
-    }
-
-    .ram.right {
-      right: 5%;
-      top: 60%;
-    }
-
-    .cloud {
-      width: 110%;
-      height: auto;
-      position: absolute;
-      top: -35%;
-      left: -3%;
-    }
-
-    .answer_1 {
-      position: absolute;
-      top: 20%;
-      left: 10%;
-    }
-
-    .answer_2 {
-      position: absolute;
-      top: 20%;
-      right: 10%;
-    }
-
-    img {
-      cursor: pointer;
-    }
+    .map-container {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    min-width: 1100px;
+    background-color:#b5d6d6;
+}
   </style>
 </head>
 <body>
-  <div class="sheeps">
-    <img src="ram-left.svg" alt="Ram left" class="ram left" />
-    <img src="ram-rigth.svg" alt="Ram right" class="ram right" />
-  </div>
-  <div class="answer">
-    <img src="circle.svg" alt="respuesta1" class="answer_1"/>
-    <img src="circle.svg" alt="respuesta2" class="answer_2"/>
-  </div>
-  <div class= "cloud">
-  <img src="cloud.svg" alt="nube" class="cloud"/>
-  </div>
+  <img src = "arena_bg.svg" class= "map-container">
 
 
   <?php
